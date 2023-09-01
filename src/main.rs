ef7d461bb74a295b0a840ae1301f9e51171f57c4
Rafał Mikrut <mikrutrafal@protonmail.com>
2023-09-01 08:20:15 +0200

    Max len

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,69 @@ use std::io::Write;
 use std::sync::atomic::AtomicUsize;
 use std::time::Duration;
 
+use anyhow::Context;
 use rayon::prelude::*;
+use reqwest::blocking::Client;
+use reqwest::header::CONTENT_LENGTH;
 
 fn main() {
-    download_links();
-    // download_packages();
+    // Set rayon thread number to 16
+    rayon::ThreadPoolBuilder::new().num_threads(16).build_global().unwrap();
+
+    // download_links();
+    download_packages();
+}
+
+const MAX_SIZE: usize = 5 * 1024 * 1024;
+
+pub fn download_header_check_size(client: &Client, package: &str, url: &str) -> anyhow::Result<(), anyhow::Error> {
+    let response = client.head(url).send().context("Error in fetching")?;
+    let Some(content_length_header) = response.headers().get(CONTENT_LENGTH) else {
+        return Ok(());
+    };
+    let content_length = content_length_header.to_str().context("Failed to parse content length header to str")?.parse::<usize>().context("Failed to parse content length header to usize")?;
+
+    if content_length > MAX_SIZE {
+        return Err(anyhow::Error::msg(format!("Ignoring too big source files {package} - {url} - {} MB", content_length / 1024 / 1024)));
+    }
+    Ok(())
+}
+
+pub fn download_single_package(package: &str, url: &str, save_location: &str) -> Result<(), anyhow::Error> {
+    let client = Client::builder().timeout(Duration::from_secs(300)).build().unwrap();
+
+    download_header_check_size(&client, package, url)?;
+
+    let res = client.get(url).send().context("Error in fetching")?;
+
+    let bytes = match res.bytes() {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            return Err(anyhow::Error::msg(format!("Failed to convert to bytes {e}")));
+        }
+    };
+
+    if bytes.len() > MAX_SIZE {
+        return Err(anyhow::Error::msg(format!("Too big file {}", bytes.len())));
+    }
+
+    let name = format!("{}{package}.tar.gz", save_location);
+    fs::write(name, bytes).unwrap();
+    Ok(())
 }
 
 pub fn download_packages() {
     let save_location = "packages/";
     let _ = fs::create_dir_all(save_location);
 
-    // Set rayon thread number to 16
-    rayon::ThreadPoolBuilder::new().num_threads(16).build_global().unwrap();
-
-    let mut packages_to_check = fs::read_to_string("links.txt").unwrap().split("\n").filter(|e| !e.trim().is_empty()).map(|idd| {
+    let mut packages_to_check = fs::read_to_string("links.txt").unwrap().split('\n').filter(|e| !e.trim().is_empty()).map(|idd| {
         let mut split = idd.split(" ||||| ");
         let package = split.next().unwrap().to_string();
         let url = split.next().unwrap().to_string();
 
         (package, url)
     }).collect::<HashSet<_>>();
-    let already_downloaded: BTreeSet<String> = fs::read_to_string("already_downloaded_zip.txt").unwrap_or_default().split("\n").map(ToString::to_string).collect::<BTreeSet<_>>();
+    let already_downloaded: BTreeSet<String> = fs::read_to_string("already_downloaded_zip.txt").unwrap_or_default().split('\n').map(ToString::to_string).collect::<BTreeSet<_>>();
 
     packages_to_check.retain(|(package, _url)| !already_downloaded.contains(package));
 
@@ -43,28 +84,10 @@ pub fn download_packages() {
             if i % 100 == 0 {
                 println!("{} / {}", i, all_to_test);
             }
-
-            let client = reqwest::blocking::Client::builder().timeout(Duration::from_secs(300)).build().unwrap();
-            let res = match client.get(&url).send() {
-                Ok(res) => res,
-                Err(e) => {
-                    println!("Error in fetching {package} - {url} - {e}");
-                    return;
-                }
-            };
-
-            let bytes = match res.bytes() {
-                Ok(bytes) => bytes,
-                Err(e) => {
-                    println!("Error in converting to bytes {package} - {url} - {e}");
-                    return;
-                }
-            };
-
-            let name = format!("{}{package}.tar.gz", save_location);
-            fs::write(&name, bytes).unwrap();
-
-            tx.send(package).unwrap();
+            match download_single_package(&package, &url, save_location) {
+                Ok(_) => tx.send(package).unwrap(),
+                Err(e) => println!("Error in downloading {package} - {url} - {e}")
+            }
         });
     });
 
@@ -77,12 +100,9 @@ pub fn download_packages() {
 }
 
 pub fn download_links() {
-    // Set rayon thread number to 16
-    rayon::ThreadPoolBuilder::new().num_threads(16).build_global().unwrap();
-
-    let mut packages_to_check = fs::read_to_string("requirements.txt").unwrap().split("\n").map(str::trim).map(ToString::to_string).collect::<HashSet<_>>();
-    let already_downloaded: BTreeSet<String> = fs::read_to_string("already_downloaded.txt").unwrap_or_default().split("\n").map(ToString::to_string).collect::<BTreeSet<_>>();
-    let not_downloaded_links: BTreeSet<String> = fs::read_to_string("not_downloaded.txt").unwrap_or_default().split("\n").map(ToString::to_string).collect::<BTreeSet<_>>();
+    let mut packages_to_check = fs::read_to_string("requirements.txt").unwrap().split('\n').map(str::trim).map(ToString::to_string).collect::<HashSet<_>>();
+    let already_downloaded: BTreeSet<String> = fs::read_to_string("already_downloaded.txt").unwrap_or_default().split('\n').map(ToString::to_string).collect::<BTreeSet<_>>();
+    let not_downloaded_links: BTreeSet<String> = fs::read_to_string("not_downloaded.txt").unwrap_or_default().split('\n').map(ToString::to_string).collect::<BTreeSet<_>>();
 
     packages_to_check.retain(|x| !already_downloaded.contains(x));
     packages_to_check.retain(|x| !not_downloaded_links.contains(x));
@@ -102,7 +122,7 @@ pub fn download_links() {
             }
 
             let client = reqwest::blocking::Client::builder().timeout(Duration::from_secs(30)).build().unwrap();
-            let Ok(res) = client.get(&format!("https://pypi.org/pypi/{package}/json")).send() else {
+            let Ok(res) = client.get(format!("https://pypi.org/pypi/{package}/json")).send() else {
                 println!("Error in fetching {package}");
                 tx.send((package, None, None)).unwrap();
                 return;
@@ -138,11 +158,13 @@ pub fn download_links() {
     let mut links_file = OpenOptions::new().append(true).create(true).open("links.txt").unwrap();
     let mut already_downloaded = OpenOptions::new().append(true).create(true).open("already_downloaded.txt").unwrap();
     let mut not_downloaded_links = OpenOptions::new().append(true).create(true).open("not_downloaded.txt").unwrap();
-    while let Ok((package,version, url)) = rx.recv() {
+    while let Ok((package, version, url)) = rx.recv() {
         if (version.is_some() && url.is_none()) || (version.is_none() && url.is_some()) {
             panic!("Invalid data for {package}");
         }
+
         if url.is_some() && version.is_some() {
+            #[allow(clippy::unnecessary_unwrap)]
             let url = url.unwrap();
             // let version = version.unwrap();
             writeln!(links_file, "{package} ||||| {url}").unwrap();
